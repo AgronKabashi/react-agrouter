6be22cf1e9f6bc9ee2c705fe9d0546bc7d163589
agronkabashi <agron@cerberussolutions.se>
2017-09-06 15:45:52 +0200

    Minor regression - misspelled export

--- build/react-agrouter.js
+++ build/react-agrouter.js
@@ -312,7 +312,7 @@ Router.childContextTypes = {
   routeIndex: PropTypes.number
 };
 
-exports.ROUTER_PRESETS = agrouter.ROUTER_PRESETS;
+exports.ROUTE_PRESETS = agrouter.ROUTE_PRESETS;
 exports.Link = Link;
 exports.Router = Router;
 exports.RouteView = RouteView;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export * from "./link";
 export * from "./router";
 export * from "./routeView";
-export { ROUTER_PRESETS } from "agrouter";
+export { ROUTE_PRESETS } from "agrouter";
